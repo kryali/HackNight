fb4b931af3c156e51c384a61059799377583ae4e
Hani Sharabash <hanibash@gmail.com>
2011-05-01 12:42:58 -0500

    misc

--- public/javascripts/fb.js
+++ public/javascripts/fb.js
@@ -7,9 +7,12 @@ $(function(){
   //Actions to take upon user entering the site
   FB.getLoginStatus(function(response) {
     if(response.session) {
+      window.token = response.session.access_token;
+      console.log(response);
       FB.api('/me', function(user) {
 
         if(user != null) {
+          console.log(user);
           //If they are already logged in, show dat face!
           $("#fb-profile > h2").append(user.name);
           $("#fb-profile > img").attr("src","http://graph.facebook.com/" + user.id + "/picture");
@@ -24,11 +27,13 @@ $(function(){
   //Actions to take upon the user logging in
   FB.Event.subscribe('auth.login', function(response) {
     if(response.session) {
+      console.log(response);
       //Hide dat button!
       $("#fb-login").css("display","none");
 
       FB.api('/me', function(user) {
         if(user != null) {
+          console.log(user);
           //If they are already logged in, show dat face!
           $("#fb-profile > img").attr("src","http://graph.facebook.com/" + user.id + "/picture");
         }
