154dbad86137436bd21210d0d21223f18eb2b25d
Kiran Ryali <kiran.ryali@gmail.com>
2011-04-30 22:09:50 -0500

    Added baseline rails stuff

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HackNight::Application.config.secret_token = '52425203dd1f6d11035c1eca48dd36a4b277dfaff80eda29a178b642fbf77095d8123930b010a658a12486e4fd14965f1564fd8c126abe39c573aea05a3f418b'
