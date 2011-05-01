4951c993278d37a7d3b736b91d7a5c9189a732b8
Kiran Ryali <kiran.ryali@gmail.com>
2011-05-01 03:13:45 -0500

    defined migrations for projects, hackers, and sponsors

--- db/migrate/20110501032627_create_sponsors.rb
+++ db/migrate/20110501032627_create_sponsors.rb
@@ -1,7 +1,10 @@
 class CreateSponsors < ActiveRecord::Migration
   def self.up
     create_table :sponsors do |t|
-
+      t.name :string
+      t.description :text
+      t.company_url :string
+      t.logo_url :string
       t.timestamps
     end
   end
--- db/migrate/20110501032648_create_projects.rb
+++ db/migrate/20110501032648_create_projects.rb
@@ -1,7 +1,11 @@
 class CreateProjects < ActiveRecord::Migration
   def self.up
     create_table :projects do |t|
-
+      t.name :string
+      t.description :text
+      t.project_url :string
+      t.image_url :string
+      t.votes :integer
       t.timestamps
     end
   end
