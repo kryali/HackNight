814dac181db06d426297493bfe20c9c457d05611
Kiran Ryali <kiran.ryali@gmail.com>
2011-05-05 07:31:48 -0500

    removed bug

--- db/migrate/20110505103944_add_event_id_to_hacknights.rb
+++
@@ -1,9 +0,0 @@
-class AddEventIdToHacknights < ActiveRecord::Migration
-  def self.up
-    add_column :hacknights, :event_id, :string
-  end
-
-  def self.down
-    remove_column :hacknights, :event_id
-  end
-end
